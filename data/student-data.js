52abc1bee178b48b608df08898dbd885d2ce9a94
JJK70 <jjk701@gmail.com>
2022-09-20 12:24:09 -0400

    update js files

--- data/student-data.js
+++ data/student-data.js
@@ -2,8 +2,8 @@ const students = [
   {text: 'John', attendance: true},
   {text: 'Sally', attendance: true},
   {text: 'Michael', attendance: false},
-  {text: 'Jessica', attendance: true,},
-  {text: 'William', attendance: false,},
+  {text: 'Jessica', attendance: true},
+  {text: 'William', attendance: false},
 ]
 
 export { 
